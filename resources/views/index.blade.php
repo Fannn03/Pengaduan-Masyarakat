79ce0becedd9cdb33dda345f9ca2f777b5288712
Fannn03 <fann.fann771@gmail.com>
2021-10-29 14:52:46 +0700

    add index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,54 @@
+@extends('templates.index')
+@section('content')
+    <div class="container-md sm:p-24 p-10 flex flex-col space-y-5 text-gray-100 bg-gray-700">
+        <h1 class="sm:text-4xl text-2xl">
+            Solusi utama menangani masalah masyarakat
+        </h1>
+        <p class="sm:text-xl sm:w-1/2 text-gray-500">
+            Mengatasi berbagai keluhan masalah masyarakat dan kejahatan kecil seperti perbaikan jalan, kemalingan, perbaikan saluran air, hingga
+            masalah lainnya.
+        </p>
+    </div>
+    <div class="sm:container mx-auto p-10 font-sans text-gray-100 sm:my-36 sm:space-y-24 flex justify-center flex-col">
+        {{-- Tentang --}}
+        <div class="flex flex-col justify-center text-center sm:space-y-5">
+            <h1 class="text-4xl">Layanan Kami</h1>
+            <p class="text-2xl text-gray-500 sm:w-1/2 sm:mx-auto">
+                100% usaha kami adalah memberikan pelayanan terbaik untuk keluhan dan pengaduan masalah anda.
+            </p>
+        </div>
+        {{-- end Tentang --}}
+        {{-- Layanan --}}
+        <div class="flex sm:flex-row sm:flex-wrap sm:justify-center gap-4 font-mono sm:w-full">
+            <div class="flex flex-col sm:w-1/4 text-center sm:space-y-5 p-5 bg-gray-900 rounded-md">
+                <h1 class="border-b border-gray-500 sm:text-xl pb-2">Perbaikan Jalan</h1>
+                <p class="text-gray-500 px-5">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, suscipit! Unde, molestias. Laboriosam aperiam, magnam reiciendis
+                </p>
+            </div>
+            <div class="flex flex-col sm:w-1/4 text-center sm:space-y-5 p-5 bg-gray-900 rounded-md">
+                <h1 class="border-b border-gray-500 sm:text-xl pb-2">Perbaikan Jalan</h1>
+                <p class="text-gray-500 px-5">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, suscipit! Unde, molestias. Laboriosam aperiam, magnam reiciendis
+                </p>
+            </div><div class="flex flex-col sm:w-1/4 text-center sm:space-y-5 p-5 bg-gray-900 rounded-md">
+                <h1 class="border-b border-gray-500 sm:text-xl pb-2">Perbaikan Jalan</h1>
+                <p class="text-gray-500 px-5">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, suscipit! Unde, molestias. Laboriosam aperiam, magnam reiciendis
+                </p>
+            </div><div class="flex flex-col sm:w-1/4 text-center sm:space-y-5 p-5 bg-gray-900 rounded-md">
+                <h1 class="border-b border-gray-500 sm:text-xl pb-2">Perbaikan Jalan</h1>
+                <p class="text-gray-500 px-5">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, suscipit! Unde, molestias. Laboriosam aperiam, magnam reiciendis
+                </p>
+            </div><div class="flex flex-col sm:w-1/4 text-center sm:space-y-5 p-5 bg-gray-900 rounded-md">
+                <h1 class="border-b border-gray-500 sm:text-xl pb-2">Perbaikan Jalan</h1>
+                <p class="text-gray-500 px-5">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, suscipit! Unde, molestias. Laboriosam aperiam, magnam reiciendis
+                </p>
+            </div>
+            
+        </div>
+
+    </div>
+@endsection
---
+++ resources/views/templates/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Pengaduan Masyarakat | Index Page</title>
+</head>
+<body class="bg-gray-800 font-mono">
+    <div class="container-md sm:p-10 py-8 px-5 flex justify-between text-white bg-gray-800">
+        <div class="flex px-5">
+            <a href="{{ route('index') }}" class="sm:text-2xl text-xl transition duration-300 hover:text-purple-500">Pengaduan Masyarakat</a >
+        </div>
+        <div class="hidden sm:flex sm:space-x-5 sm:px-5">
+            <a href="" class="text-xl transition duration-300 hover:text-purple-500">Home</a>
+            <a href="" class="text-xl transition duration-300 hover:text-purple-500">Tentang Kami</a>
+            <a href="" class="text-xl transition duration-300 hover:text-purple-500">Layanan</a>
+            <a href="" class="text-xl transition duration-300 hover:text-purple-500">Login</a>
+            <a href="" class="text-xl transition duration-300 hover:text-purple-500">Register</a>
+        </div>
+    </div>
+    @yield('content')  
+</body>
+</html>
