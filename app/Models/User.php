63d725a9f78dccf32240ed85802681a2933e75f1
Fannn03 <fann.fann771@gmail.com>
2021-10-29 15:57:14 +0700

    add fillable username, foto, jabatan, aktivasi

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,12 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'username',
+        'foto',
         'email',
         'password',
+        'jabatan',
+        'aktivasi',
     ];
 
     /**
