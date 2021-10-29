d5a4febc592edf453f32e70e0a8773028a430a4f
Fannn03 <fann.fann771@gmail.com>
2021-10-29 17:51:05 +0700

    add dashboard page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,4 +24,11 @@ Route::middleware('guest')->group(function(){
     Route::get('/register', [RegisterController::class, 'view'])->name('register');
 
     Route::post('/register', [RegisterController::class, 'store']);
+    Route::post('/login', [LoginController::class, 'login']);
+});
+
+Route::middleware(['auth'])->group(function (){
+    Route::prefix('dashboard')->group(function (){
+        Route::get('/', [DashboardController::class, 'index'])->name('index-dashboard');
+    });
 });
