50b37d1e678a274667d6da821293639d3004d87f
Fannn03 <fann.fann771@gmail.com>
2021-10-29 14:02:58 +0700

    first migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama');
+            $table->string('username');
+            $table->string('foto')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('jabatan', ['masyarakat', 'petugas', 'admin']);
+            $table->boolean('aktivasi');
             $table->rememberToken();
             $table->timestamps();
         });
