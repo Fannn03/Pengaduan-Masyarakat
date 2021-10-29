c917a4ef6ed475ea95583cbbf35626ed414747a7
Fannn03 <fann.fann771@gmail.com>
2021-10-29 15:26:18 +0700

    add login page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [IndexController::class, 'index'])->name('index')->middleware('guest');
+
+Route::middleware('guest')->group(function(){
+    Route::get('/login', [LoginController::class, 'view'])->name('login');
+});
